da97b970ab7d34a143f63b793ed1968e529b471a
EA <ea@EAs-iMac.localdomain>
2023-09-05 12:45:59 +0700

    Update Pipfile.lock for selenium testing

--- city_scrapers/spiders/ind_admin_finance.py
+++ city_scrapers/spiders/ind_admin_finance.py
@@ -9,7 +9,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.utils import ChromeType
+from webdriver_manager.core.os_manager import ChromeType
 
 
 class IndAdminFinanceSpider(CityScrapersSpider):
